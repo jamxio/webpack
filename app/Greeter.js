532f307bda478071bc124e90d075460d13a31f95
jamxio <jamxio-w@hotmail.com>
2018-08-27 11:40:35 +0800

    webpack 练习

---
+++ app/Greeter.js
@@ -0,0 +1,6 @@
+// Greeter.js
+module.exports = function() {
+  var greet = document.createElement('div');
+  greet.textContent = "Hi there is my greetings and the greetings is not for you!";
+  return greet;
+};
